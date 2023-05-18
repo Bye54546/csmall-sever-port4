8cd19edce3fabfc8153c9b99f17c46aeaa1074b6
19668 <1>
2023-05-18 16:25:50 +0800

    Part 4,05-18

--- csmall-product/src/main/java/cn/tedu/csmall/product/ex/ServiceException.java
+++ csmall-product/src/main/java/cn/tedu/csmall/product/ex/ServiceException.java
@@ -12,16 +12,4 @@ public class ServiceException extends RuntimeException {
         super(message);
         this.serviceCode = serviceCode;
     }
-
-    public ServiceException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public ServiceException(Throwable cause) {
-        super(cause);
-    }
-
-    public ServiceException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
-    }
 }
